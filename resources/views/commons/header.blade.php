90347e7093c7701c90820c29349c179d4080ad17
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-03 17:06:51 +0100

    creo header e jumbo

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,9 +1,19 @@
 <header>
     <div class="container">
-        <img src="../assets/img/dc-logo.png" alt="">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="">
         <ul>
-            <li></li>
+            <li><a href="#">Carachter</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
         </ul>
     </div>
+    <div class="jumbo"></div>
 </header>
 

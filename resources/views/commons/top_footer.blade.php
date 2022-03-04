7c910024275440fa572db1c92ab8f380bf040dc6
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-04 10:24:15 +0100

    mod route

--- resources/views/commons/top_footer.blade.php
+++ resources/views/commons/top_footer.blade.php
@@ -1,11 +1,12 @@
 <div class="top_footer">
     <div class="container">
         @foreach ($topfooter as $key => $valore)
-        <div class="content_top">
-            <img src="" >
-            <span>{{$valore['text']}}</span>
-        </div>
-        @endforeach
+            <div class="content_top">
+                <img src="" >
+                <span>{{$valore['text']}}</span>
+            </div>
+        @endforeach 
+        
     </div>
 </div>
             
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $data_comics = config('comics');
     $nav_data = config('nav_data');
-    $foot_top_data = config('topf_data');
-    return view('comic',['comics'=>$data_comics],['nav'=>$nav_data],['topfooter'=>$foot_top_data]);
+    $footer_top_data = config('top_footer_data');
+    return view('comic',['comics'=>$data_comics,'nav'=>$nav_data,'topfooter'=>$footer_top_data]);
 });
+
+

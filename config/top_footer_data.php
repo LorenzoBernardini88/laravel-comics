73f6c5366fc4752ae0c6e82e4b91ee72f7519f95
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-04 11:18:15 +0100

    top_footer

--- config/top_footer_data.php
+++ config/top_footer_data.php
@@ -2,22 +2,22 @@
 return [
     [
     "text" => "DIGITAL COMICS",
-    "img" => ("../assets/img/buy-comics-digital-comics.png"),
+    "img" => ("img/buy-comics-digital-comics.png"),
     ],
     [
     "text" => "DC MERCHANDISE",
-    "img" => ("../assets/img/buy-comics-merchandise.png"),
+    "img" => ("img/buy-comics-merchandise.png"),
     ],
     [
     "text" => "COMIC SHOP LOCATOR",
-    "img" => ("../assets/img/buy-comics-subscriptions.png"),
+    "img" => ("img/buy-comics-subscriptions.png"),
     ],
     [
     "text" => "SUBSCRIPTIONS",
-    "img" => ("../assets/img/buy-comics-shop-locator.png"),
+    "img" => ("..img/buy-comics-shop-locator.png"),
     ],
     [
     "text" => "DC POWER VISA",
-    "img" => ("../assets/img/buy-dc-power-visa.svg"),
+    "img" => ("img/buy-dc-power-visa.svg"),
     ],
 ];
--- resources/views/commons/top_footer.blade.php
+++ resources/views/commons/top_footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container">
         @foreach ($topfooter as $key => $valore)
             <div class="content_top">
-                <img src="" >
+                <img src="{{$valore['img']}}" >
                 <span>{{$valore['text']}}</span>
             </div>
         @endforeach 
-        
     </div>
 </div>
+        
             
